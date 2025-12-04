88105e3c759cb8861b80a205325c02bc1a06b2be
abood2233ab <abdoza855@gmail.com>
2025-12-04 21:54:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,9 +18,7 @@ function draw() {
     for (let i = 0; i < drops.length; i++) {
         const text = letters.charAt(Math.floor(Math.random() * letters.length));
         ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-
-        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975)
-            drops[i] = 0;
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) drops[i] = 0;
         drops[i]++;
     }
 }
